14977d3fc2dd5bc56a420c7a3739f94a491ad63b
Wes Cole <wesley.cole87@gmail.com>
2021-05-04 01:12:05 -0400

    Remove stuff that isn't needed for example

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,39 @@
-import styles from "../styles/Home.module.css";
 import { Component } from "../components/Component";
 
 export default function Home() {
   return (
-    <>
-      <div className={styles.container}>
-        <div
-          css={{
-            display: "flex",
+    <div>
+      <div
+        css={{
+          display: "flex",
 
-            mx: false ? "1" : 2,
-            p: "3",
-            px: 4,
+          mx: false ? "1" : 2,
+          p: "3",
+          px: 4,
 
-            border: "1px solid --color-blue-100",
-            // borderColor: 'blue-100',
-            color: "white",
-            bg: "blue-500",
-            borderRadius: "md",
-            boxShadow: "lg",
-            fontWeight: "bold",
-            "@media:sm": {
-              background: "blue-500",
-            },
-            md: {
-              backgroundImage:
-                "linear-gradient(to right, --color-blue-400, --color-indigo-800)",
-            },
-            "@media:xl": {
-              backgroundImage:
-                "linear-gradient(to right, --color-red-400, --color-indigo-800)",
-            },
-          }}
-        >
-          emotion time
-        </div>
-        <div style={{ color: "blue-500" }}>style only</div>
-        <Component />
+          border: "1px solid --color-blue-100",
+          color: "white",
+          bg: "blue-500",
+          borderRadius: "md",
+          boxShadow: "lg",
+          fontWeight: "bold",
+          "@media:sm": {
+            background: "blue-500",
+          },
+          md: {
+            backgroundImage:
+              "linear-gradient(to right, --color-blue-400, --color-indigo-800)",
+          },
+          "@media:xl": {
+            backgroundImage:
+              "linear-gradient(to right, --color-red-400, --color-indigo-800)",
+          },
+        }}
+      >
+        emotion time
       </div>
-    </>
+      <div style={{ color: "blue-500" }}>style only</div>
+      <Component />
+    </div>
   );
 }
